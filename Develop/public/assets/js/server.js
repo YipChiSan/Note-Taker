53c9063f1f9f44afc08ba434230f2a98acc9aac7
YipChiSan <zye39@yahoo.com>
2022-08-06 23:57:35 +1000

    Let backend display index.html and notes.html file

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '../../index.html')));
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '../../notes.html')));
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT} 🚀`));
